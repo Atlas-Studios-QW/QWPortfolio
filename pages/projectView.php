0851d65ebe1c4f9d0bd0976a538d59ecc8a43161
QuillanW <quillanwielhouwer@live.nl>
2022-12-28 18:50:04 +0100

    Update

--- pages/projectView.php
+++ pages/projectView.php
@@ -9,10 +9,23 @@ $projectID = $_GET["projectID"];
 <title></title>
 
 <div class="content">
-    <div class="PJmain">
-
-    </div>
-    <div class="PJside">
-
+    <div class="PJbody">
+        <div class="PJhead">
+            <h1>Title</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quam dolor illo sed laborum illum blanditiis possimus corporis architecto id aliquam commodi molestiae, velit quisquam unde pariatur, non temporibus? Eos saepe quas repellendus quos blanditiis ipsam id excepturi cumque nihil veritatis totam sapiente suscipit numquam, nemo pariatur maxime corporis magnam dolorem dignissimos harum esse? Autem qui vel fugiat eaque nesciunt consectetur iure ut nemo illum veritatis, sint tempore ab deserunt recusandae deleniti perspiciatis hic quas delectus dolore quae eos. Vero inventore, enim numquam id autem, quis cupiditate impedit tempore accusantium est beatae velit quisquam ducimus repellendus obcaecati excepturi accusamus iusto.</p>
+            <div class="PJlinks">
+                <a href="#">Link1</a><br>
+                <a href="#">Link2</a><br>
+                <a href="#">Link3</a><br>
+            </div>
+        </div>
+        <div class="PJmain">
+            <div class="CLcard">
+                
+            </div>
+        </div>
+        <div class="PJside">
+    
+        </div>
     </div>
 </div>
